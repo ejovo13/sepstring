0dfda3e40de8ddf63879c0662e650f48a508df6c
Evan Voyles <ejovo13@yahoo.com>
2024-12-04 02:47:02 +0100

    change name back to default package

--- flake.nix
+++ flake.nix
@@ -12,13 +12,13 @@
       # create a custom "mkPoetryApplication" API function that under the hood uses
       # the packages and versions (python3, poetry etc.) from our pinned nixpkgs above:
       inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; }) mkPoetryApplication;
-      sepString = mkPoetryApplication { projectDir = ./.; };
+      defaultPackage = mkPoetryApplication { projectDir = ./.; };
     in
     {
       apps.${system}.default = {
         type = "app";
         # replace <script> with the name in the [tool.poetry.scripts] section of your pyproject.toml
-        program = "${sepString}/bin/sep";
+        program = "${defaultPackage}/bin/sep";
       };
     };
 }
